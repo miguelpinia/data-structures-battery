da5c360f3a3c6ef0ca268efd485b18baebf53fab
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2022-01-27 20:17:43 -0600

    Add new printer function.

--- main/utils.cpp
+++ main/utils.cpp
@@ -13,3 +13,12 @@ void print_time(std::clock_t time, double duration, int value) {
               << "Wall clock time passed: " << duration << " ms\n"
               << value << " max value stored\n";
 }
+
+void print_time(std::clock_t time, long duration, int value) {
+    // Printing times
+    // https://pythonspeed.com/articles/blocking-cpu-or-io/
+    std::cout << std::fixed << std::setprecision(2) << "CPU time used: "
+              << 1000.0 * (time) / CLOCKS_PER_SEC << " ms\n"
+              << "Wall clock time passed: " << duration << " ns\n"
+              << value << " max value stored\n";
+}
--- main/utils.hpp
+++ main/utils.hpp
@@ -1,3 +1,5 @@
 #include <chrono>
 
 void print_time(std::clock_t time, double duration, int value);
+
+void print_time(std::clock_t time, long duration, int value);
