5dac3ee34bf8802ded34c96cc0d2b558e6be3333
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2024-01-14 12:32:52 -0600

    ref: Adds hazard pointers to the lock-free queue.

--- queueTest/include/HPQueue.hpp
+++ queueTest/include/HPQueue.hpp
@@ -2,6 +2,7 @@
 #define _HP_Queue_HPP_
 
 #include <atomic>
+#include "MemoryManagementPool.hpp"
 
 template <typename T>
 struct Node {
@@ -15,9 +16,12 @@ private:
     Node<T> dummy;
     std::atomic<Node<T>*> Head{&dummy};
     std::atomic<Node<T>*> Tail{&dummy};
+    MemoryManagementPool<Node<T>> mm;
 
 public:
-    void enqueue(T data) {
+    Queue() {}
+
+    void enqueue(T data, const int thread_id) {
         Node<T>* node = new Node<T>();
         node->data = data;
         node->next = nullptr;
@@ -25,7 +29,8 @@ public:
         Node<T>* next = nullptr;
         Node<T>* null = nullptr;
         while (true) {
-            t = Tail.load();
+            t = mm.protect(0, Tail, thread_id);
+            if (Tail != t) continue;
             next = Tail.load()->next.load();
             if (Tail != t) continue;
             if (next != nullptr) {
@@ -38,15 +43,16 @@ public:
         Tail.compare_exchange_strong(t, node);
     }
 
-    T dequeue() {
+    T dequeue(const int thread_id) {
         T data;
         Node<T>* h = nullptr;
         Node<T>* t = nullptr;
         Node<T>* next = nullptr;
         while (true) {
-            h = Head.load();
+            h = mm.protect(0, Head, thread_id);
+            if (Head != h) continue;
             t = Tail.load();
-            next = h->next.load();
+            next = mm.protect(1, h->next, thread_id);
             if (Head != h) continue;
             if (next == nullptr) return nullptr;
             if (h == t) {
@@ -56,6 +62,7 @@ public:
             data = next->data;
             if (Head.compare_exchange_strong(h, next)) break;
         }
+        mm.retire(h, thread_id);
         return data;
     }
 };
