030ab839d3f57730d74308f9a2ec0961bddba3ff
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2024-01-17 09:59:29 -0600

    ref: Extract function for testing to function template.

--- queueTest/include/ThreadingQueue.hpp
+++ queueTest/include/ThreadingQueue.hpp
@@ -11,33 +11,37 @@
 #include <random>
 #include <cstdlib>
 #include "HPQueue.hpp"
+#include "FAAArrayQueue.hpp"
 #include "include/utils.hpp"
 
-long queue_test(int cores) {
+template <typename Queue>
+long queue_test_general(int cores, Queue &queue) {
     std::clock_t c_start = std::clock();
     auto t_start = std::chrono::high_resolution_clock::now();
-    Queue<std::string> queue;
     std::vector<std::thread> threads;
     int operations = 10'000'000 / cores;
     auto wait_for_begin = []() noexcept {};
     std::barrier sync_point(cores, wait_for_begin);
-
-    std::function<void()> func = [&] () {
+    std::function<void(int)> func = [&] (const int thread_id) {
         std::random_device rd;
         std::mt19937 gen(rd());
         std::uniform_int_distribution<> distrib(1, 3);
         sync_point.arrive_and_wait();
+        std::string foo;
         for (int i = 0; i < operations; i++) {
-            if (i % 2 == 0) {
-                queue.enqueue(std::to_string(i));
-            } else {
-                queue.dequeue();
-            }
-            for (int j = 0; j < 80; j = j + distrib(gen)) {}
+            for (int j = 0; j < 40; j = j + distrib(gen)) {}
+            std::string* foo = new std::string();
+            *foo = std::to_string(i);
+            queue.enqueue(foo, thread_id);
+        }
+        for (int i = 0; i < operations; i++) {
+            for (int j = 0; j < 40; j = j + distrib(gen)) {}
+            queue.dequeue(thread_id);
         }
     };
     for (int i = 0; i < cores; i++) {
-        threads.push_back(std::thread(func));
+        const int thread_id = i;
+        threads.push_back(std::thread(func, thread_id));
         cpu_set_t cpuset;
         CPU_ZERO(&cpuset);
         CPU_SET(i, &cpuset);
